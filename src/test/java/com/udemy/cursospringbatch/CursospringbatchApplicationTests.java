5f35eb928f11645174effdc68e11984490085370
unoesc <unoesc@Ubuntu-academico.myguest.virtualbox.org>
2023-07-31 09:20:36 -0300

    Initial commit

---
+++ src/test/java/com/udemy/cursospringbatch/CursospringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.cursospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
