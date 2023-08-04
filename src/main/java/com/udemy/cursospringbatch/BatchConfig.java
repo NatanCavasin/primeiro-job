5ceffac56e364f7ce15a9701f9a897bc9205eecf
Natan Cavasin <natancavasin@hotmail.com>
2023-08-04 16:44:05 -0300

    feat: created the Job using chunk step

--- src/main/java/com/udemy/cursospringbatch/BatchConfig.java
+++ src/main/java/com/udemy/cursospringbatch/BatchConfig.java
@@ -1,5 +1,8 @@
 package com.udemy.cursospringbatch;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -10,6 +13,11 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -55,4 +63,41 @@ public class BatchConfig {
 			}
 		};
 	}
+	
+	
+	
+	@Bean
+	public Job imprimeParImparJob() {
+		return jobBuilderFactory
+				.get("imprimeParImparJob") //Nome do Job
+				.start(imprimeParImparStep()) //steps
+				.incrementer(new RunIdIncrementer()) // Adiciona um run ID a acda execução, deste modo o Job pode ser executado mais de uma vez.
+				.build();
+		// O incrementador somente pode ser utilizado se o Job não precisa ser reinicializado
+	}
+	
+	public Step imprimeParImparStep() {
+		return stepBuilderFactory
+				.get("imprimeParImparStep")
+				.<Integer, String>chunk(1)
+				.reader(contaAteDezReader())
+				.processor(parOuImparProcessor())
+				.writer(imprimeWriter())
+				.build();
+	}
+
+	public IteratorItemReader<Integer> contaAteDezReader() {
+		List<Integer> numeros = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		return new IteratorItemReader<Integer>(numeros.iterator());
+	}
+	
+	public FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+		return new FunctionItemProcessor<Integer, String>  
+		(item -> item % 2 == 0 ? String.format("Item %s é Par", item) : String.format("Item %s é Impar", item));
+	}
+	
+	public ItemWriter<String> imprimeWriter() {
+		return itens -> itens.forEach(System.out::println);
+	}
+	
 }
